9072575698c3cb131a84697ed71b724f536c67b9
Miguel Conde Santos <miguelconde121004@gmail.com>
2025-09-22 21:52:50 -0300

    Add configuração de conexão do bd, importação dos dados tratados para o banco e o main para as rotas da API

--- src/db.py
+++ src/db.py
@@ -1,2 +1,17 @@
 import psycopg2
-import pandas as pd
+
+DB_CONFIG = {
+    "host": "localhost", #coloque o host do seu banco
+    "port": 5432, #coloque a porta do seu banco (o padrão do PostgreSQL é 5432)
+    "dbname": "domrock", #coloque o nome do seu banco
+    "user": "postgres", #coloque o usuário do seu banco
+    "password": "root" #coloque a senha do seu usuário configurado no banco de dados
+}
+
+def get_connection():
+    try:
+        print("Conexão feita")
+        return psycopg2.connect(**DB_CONFIG)
+    except Exception as e:
+        print(f"Erro ao conectar ao banco de dados: {e}")
+        raise
